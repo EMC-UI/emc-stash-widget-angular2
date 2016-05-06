448f4116b2c16f35896005b057bf0d04905fa69c
Dehru Cromer <dehru.cromer@emc.com>
2016-05-05 21:58:00 -0600

    fix typescript errors

--- app/stats.model.ts
+++ app/stats.model.ts
@@ -1,14 +1,19 @@
 import {iStashModel} from "./iStashModel";
+import {iProjectModel} from "./iProjectModel";
 /**
  * Created by cromed on 5/5/16.
  */
 export class StatsModel implements iStashModel {
+  _id:string;
   name:string;
   count:number;
-  projects:String[];
+  projects:iProjectModel[];
   constructor() {
+    this._id = 'SKUI';
     this.name = 'Fake User';
     this.count = 1;
-    this.projects = ['project1', 'project2'];
+    this.projects = [
+
+    ];
   }
 }
